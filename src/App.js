588bb87daca6b0a90f0fb0ad1ce3aa9543dbdeea
Felertooshia <67173720+Felertooshia@users.noreply.github.com>
2020-10-28 20:07:36 +0800

    Moved Pages to src

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import "./App.css";
+// import Nav from "./components/Nav/Nav"
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Router>
+            <div>
+                {/* <header>
+                    
+                    <Nav username={username} />
+                </header> */}
+
+                <br></br>
+                <Switch>
+                    {/* <Route path="/login/">
+                        <LoginPage />
+                    </Route> */}
+
+                    <Route exact path="/">
+                        <HomePage />
+                    </Route>
+                </Switch>
+            </div>
+
+        </Router>
+
+    )
 }
 
 export default App;
