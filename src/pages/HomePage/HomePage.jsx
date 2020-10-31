8d96c06fd6537f600f6c87a337703cbf83cc1c56
Felertooshia <67173720+Felertooshia@users.noreply.github.com>
2020-10-31 12:18:23 +0800

    Updated HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import { useHistory } from "react-router-dom";
 import "./HomePage.css";
 import "../SignUp/SignUp";
 import "../ContactUs/ContactUs";
@@ -9,39 +8,6 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
 
-  // const [credentials, setCredentials] = useState({
-  //   username: "",
-  //   email: "",
-  //   preferred_name: "",
-  //   password: "",
-  //   userprofile: {},
-  // });
-
-  // const history = useHistory();
-
-  // const postData = async () => {
-  //   //function you can call but carry on as well
-  //   const response = await fetch(`${process.env.REACT_APP_API_URL}users/`, {
-  //     method: "post",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(credentials),
-  //   });
-  //   return response.json();
-  // };
-
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (credentials.username != null) {
-  //     console.log(credentials);
-
-  //     postData().then((response) => {
-  //       history.push("/login");
-  //     });
-  //   }
-  // };
     return (
         <div className="homepage">
           <div className="logo" style={{flex:1, backgroundColor:"pink"}}>
@@ -63,7 +29,5 @@ function HomePage() {
         </div>
     );
 }
-
-
 
 export default HomePage
