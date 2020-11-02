eaefd62404cc838b9124803ed9fd5509a8d139cc
Vivianne <viviannevilar@gmail.com>
2020-11-02 16:51:19 +0800

    Delete button for collection list view

--- src/components/CollectionCard/CollectionCard.jsx
+++ src/components/CollectionCard/CollectionCard.jsx
@@ -3,9 +3,15 @@ import "./CollectionCard.css"
 
 
 function CollectionCard(props) {
+
+    //////////////////////////// variables ////////////////////////////
     const { collectionData } = props
     const id = collectionData.id
 
+
+    //////////////////////////// methods ////////////////////////////
+
+    // Archive collection
     const archiveCollection = async (e) => {
         e.preventDefault();
         let token = window.localStorage.getItem("token");
@@ -22,19 +28,39 @@ function CollectionCard(props) {
         return response.json();
     }
 
+    // Delete collection
+    
+    const deleteCollection = async (e) => {
+        e.preventDefault();
+        let token = window.localStorage.getItem("token");
+        let urlPath = "collection/" + collectionData.id
+    
+        const response = await fetch(`${process.env.REACT_APP_API_URL}${urlPath}/`, {
+            method: "delete",
+            headers: {
+                Authorization: `Token ${token}`,
+            },
+        });
+
+        window.location.reload();
+    }
+
+    // Share collection
     function shareCollection()  {
 
-        const linkText = "https://warm-falls-74169.herokuapp.com/collection/shared/" + collectionData.signed_pk + "/"
+        const linkText = "https://warm-falls-74169.herokuapp.com/collection/" + collectionData.signed_pk + "/"
 
         navigator.clipboard.writeText(linkText)
 
     }
 
+     //////////////////////////// return ////////////////////////////   
     return (
         <div className="collection-wrapper">
             <p> {collectionData.title}
             <button onClick={archiveCollection}>{ collectionData.is_active ? "Archive" : "Unarchive" }</button> 
             <button onClick={shareCollection}>Share Collection</button>
+            <button onClick={deleteCollection}>Delete Collection</button> 
             </p>
 
         </div>
